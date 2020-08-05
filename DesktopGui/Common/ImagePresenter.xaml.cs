93653d76c1be99639ef5842842c65eb2c89df049
snorek55 <40332063+snorek55@users.noreply.github.com>
2020-08-05 16:11:19 +0200

    Created ImagePresenter for image data

---
+++ DesktopGui/Common/ImagePresenter.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace DesktopGui.Common
+{
+	/// <summary>
+	/// Lógica de interacción para ImagePresenter.xaml
+	/// </summary>
+	public partial class ImagePresenter : UserControl
+	{
+		public ImagePresenter()
+		{
+			InitializeComponent();
+		}
+
+		private void ListView_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			PART_ScrollViewer.ScrollToHome();
+		}
+	}
+}
