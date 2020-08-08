223d4e1b152cc14209f9eaa6e4c90d178cafb04c
snorek55 <40332063+snorek55@users.noreply.github.com>
2020-08-08 17:31:43 +0200

    Improved adaptares tests with fluentassertions.

--- UnitTests/Unit/AdaptersTests.cs
+++ UnitTests/Unit/AdaptersTests.cs
@@ -2,6 +2,8 @@
 
 using EntryPoint.Mapper;
 
+using FluentAssertions;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Moq;
@@ -17,6 +19,7 @@ using Organizers.MovOrg.UseCases.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Data;
 
 namespace Tests.Unit
 {
@@ -24,68 +27,127 @@ namespace Tests.Unit
 	public class AdaptersTests
 	{
 		private MoviesSectionViewModel moviesSectionViewModel;
-		private Mock<IMoviesService> mockMoviesService;
-		private Mock<IDispatcher> mockDispatcher;
+		private Mock<IMoviesService> mockMoviesService = new Mock<IMoviesService>();
+		private Mock<IDispatcher> mockDispatcher = new Mock<IDispatcher>();
 		private IAutoMapper autoMapper = new MapperImpl();
 		private Fixture fixture = new Fixture();
 
 		private List<Movie> moviesInLocal;
-		private List<MovieViewModel> moviesVMInLocal => autoMapper.Map<List<MovieViewModel>>(moviesInLocal);
+		private List<MovieViewModel> MoviesVMInLocal => autoMapper.Map<List<MovieViewModel>>(moviesInLocal);
 
+		private GetAllMoviesFromLocalResponse response;
+
+		//Must setup for constructor in MoviesSection
 		public AdaptersTests()
 		{
-			fixture.Behaviors.Add(new OmitOnRecursionBehavior());
-			mockMoviesService = new Mock<IMoviesService>();
-			var response = fixture.Create<GetAllMoviesFromLocalResponse>();//new GetAllMoviesFromLocalResponse(movies);
-																		   //Must setup for constructor in MoviesSection
+			mockDispatcher.Setup(x => x.BeginInvoke(It.IsAny<Action>())).Callback<Action>(a => a.Invoke());
+		}
 
+		[TestInitialize]
+		public void Initialize()
+		{
+			fixture = new Fixture();
+			fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+			response = fixture.Create<GetAllMoviesFromLocalResponse>();
 			moviesInLocal = response.Movies.ToList();
 			mockMoviesService.Setup(x => x.GetAllMoviesFromLocal()).ReturnsAsync(response);
-			mockDispatcher = new Mock<IDispatcher>();
-			mockDispatcher.Setup(x => x.BeginInvoke(It.IsAny<Action>())).Callback<Action>(a => a.Invoke());
 			moviesSectionViewModel = new MoviesSectionViewModel(mockMoviesService.Object, autoMapper, mockDispatcher.Object);
 		}
 
+		#region SearchCommandTests
+
 		[TestMethod]
-		public void SearchCommand_ShouldAddMoviesWithSameTitle_WhenExecuted()
+		public void SearchCommand_ShouldAddMoviesWithSameTitle_WhenTheyHaveNotBeenAlreadySearched()
 		{
-			var response = new GetSuggestedTitleMoviesResponse(moviesInLocal);
-			fixture.Register(() => "Title");
+			var response = fixture.Build<GetSuggestedTitleMoviesResponse>().With(x => x.AlreadySearched, false).Create();
+
 			var searchWord = fixture.Create<string>();
 			mockMoviesService.Setup(x => x.GetMoviesFromSuggestedTitleAsync(searchWord, false)).ReturnsAsync(response);
 			moviesSectionViewModel.SuggestedTitle = searchWord;
+			moviesSectionViewModel.Movies.Should().BeEquivalentTo(MoviesVMInLocal);
 			moviesSectionViewModel.SearchCommand.Execute("");
 
-			Assert.AreEqual(moviesVMInLocal.Count, moviesSectionViewModel.Movies.Count);
-			CollectionAssert.AreEquivalent(moviesVMInLocal, moviesSectionViewModel.Movies.ToList());
+			var expectedVms = autoMapper.Map<List<MovieViewModel>>(response.Movies);
+			moviesSectionViewModel.Movies.Should().BeEquivalentTo(expectedVms);
 		}
+
+		[TestMethod]
+		public void SearchCommand_ShouldFilterMoviesWithSameTitle_WhenAlreadySearched()
+		{
+			var response = fixture.Build<GetSuggestedTitleMoviesResponse>()
+				.With(x => x.AlreadySearched, true)
+				.With(x => x.Movies, moviesInLocal)
+				.Create();
+
+			fixture.Register(() => "a5");
+			var searchWord = fixture.Create<string>();
+			mockMoviesService.Setup(x => x.GetMoviesFromSuggestedTitleAsync(searchWord, false)).ReturnsAsync(response);
+			moviesSectionViewModel.SuggestedTitle = searchWord;
+			moviesSectionViewModel.Movies.Should().BeEquivalentTo(MoviesVMInLocal);
+			moviesSectionViewModel.SearchCommand.Execute("");
+
+			var moviesWithCorrectTitle = response.Movies.ToList().FindAll(x => x.Title.Contains(searchWord) || x.Description.Contains(searchWord));
+			var expectedVms = autoMapper.Map<List<MovieViewModel>>(moviesWithCorrectTitle);
+
+			var collectionView = CollectionViewSource.GetDefaultView(moviesSectionViewModel.Movies);
+
+			collectionView.Should().HaveSameCount(expectedVms);
+			collectionView.Should().BeEquivalentTo(expectedVms);
+		}
+
+		[TestMethod]
+		public void SearchCommand_ShouldReturnNoMovies_WhenTitleDoesNotExist_AndIsNotAlreadySearched()
+		{
+			var response = fixture.Build<GetSuggestedTitleMoviesResponse>()
+				.With(x => x.Movies, new List<Movie>())
+				.With(x => x.AlreadySearched, false)
+				.Create();
+			var searchWord = fixture.Create<string>();
+			mockMoviesService.Setup(x => x.GetMoviesFromSuggestedTitleAsync(searchWord, false)).ReturnsAsync(response);
+			moviesSectionViewModel.SuggestedTitle = searchWord;
+			moviesSectionViewModel.Movies.Should().BeEquivalentTo(MoviesVMInLocal);
+			moviesSectionViewModel.SearchCommand.Execute("");
+
+			moviesSectionViewModel.Movies.Should().BeEmpty();
+		}
+
+		#endregion SearchCommandTests
+
+		#region ClearSearchCommandTests
 
 		[TestMethod]
 		public void ClearSearchCommand_ShouldShowMoviesFromLocal_WhenExecuted()
 		{
-			var response = new GetAllMoviesFromLocalResponse(new List<Movie>() { new Movie { Id = "TestMovie", Title = "Test" }, new Movie { Id = "TestMovie2", Title = "TestMovie2" } });
+			var response = new GetAllMoviesFromLocalResponse(moviesInLocal);
 			mockMoviesService.Setup(x => x.GetAllMoviesFromLocal()).ReturnsAsync(response);
+
+			moviesSectionViewModel.Movies.Clear();
+
+			moviesSectionViewModel.Movies.Should().BeEmpty();
 
 			moviesSectionViewModel.ClearSearchCommand.Execute("");
 
-			Assert.AreEqual(2, moviesSectionViewModel.Movies.Count);
-			Assert.AreEqual("TestMovie", moviesSectionViewModel.Movies[0].Id);
+			moviesSectionViewModel.Movies.Should().BeEquivalentTo(MoviesVMInLocal);
 		}
+
+		#endregion ClearSearchCommandTests
+
+		#region Top250CommandTests
 
 		[TestMethod]
 		public void Top250Command_ShouldUpdateMoviesAndShowAllFromLocal_WhenExecuted()
 		{
-			var movies = new List<Movie>() { new Movie { Id = "TestMovie", Title = "Test" }, new Movie { Id = "TestMovie2", Title = "TestMovie2" } };
 			var updateResponse = new UpdateTopMoviesResponse();
-			var localResponse = new GetAllMoviesFromLocalResponse(movies);
-			mockMoviesService.Setup(x => x.UpdateTopMovies()).Callback(() => movies[0].Rank = 2).ReturnsAsync(updateResponse);
-			mockMoviesService.Setup(x => x.GetAllMoviesFromLocal()).ReturnsAsync(localResponse);
+			mockMoviesService.Setup(x => x.UpdateTopMovies()).Callback(() => response.Movies.First().Rank = 2).ReturnsAsync(updateResponse);
 
+			moviesSectionViewModel.Movies.Should().BeEquivalentTo(MoviesVMInLocal);
+			moviesSectionViewModel.Movies[0].Rank.Should().NotBe(2);
 			moviesSectionViewModel.Top250Command.Execute("");
 
-			Assert.AreEqual(2, moviesSectionViewModel.Movies.Count);
-			Assert.AreEqual("TestMovie", moviesSectionViewModel.Movies[0].Id);
-			Assert.AreEqual(2, moviesSectionViewModel.Movies[0].Rank);
+			moviesSectionViewModel.Movies.Should().BeEquivalentTo(MoviesVMInLocal);
+			moviesSectionViewModel.Movies[0].Rank.Should().Be(2);
 		}
+
+		#endregion Top250CommandTests
 	}
 }
