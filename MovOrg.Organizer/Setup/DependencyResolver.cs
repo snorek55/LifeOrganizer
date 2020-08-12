8e3fb2fce0835f10058247694e210410b5f1788f
snorek55 <40332063+snorek55@users.noreply.github.com>
2020-08-12 15:53:28 +0200

    Reorganized mapper config and created plugin data for mef

--- MovOrg.Organizer/Setup/DependencyResolver.cs
+++ MovOrg.Organizer/Setup/DependencyResolver.cs
@@ -3,20 +3,23 @@ using Common.Setup;
 
 using Microsoft.Extensions.DependencyInjection;
 
+using MovOrg.Organizer.Setup;
 using MovOrg.Organizers.Adapters.Container;
 using MovOrg.Organizers.Adapters.Sections;
 using MovOrg.Organizers.UseCases;
 
 namespace MovOrg.Organizers.Setup
 {
-	public class MovOrgOrganizerDependencyResolver : IDependencyResolver
+	public class DependencyResolver : IDependencyResolver
 	{
 		public void Setup(IServiceCollection services)
 		{
+			services.AddSingleton<IProfilePluginData, ProfilePluginData>();
+			services.AddSingleton<IContainerPluginData, ContainerPluginData>();
 			services.AddSingleton<IMoviesService, MoviesService>();
 			services.AddSingleton<ISectionsFactory, MovOrgSectionsFactory>();
 			services.AddSingleton<MovOrgContainerViewModel>();
-			services.AddSingleton<MovOrgMainMenuItemViewModel>();
+			services.AddSingleton<ContainerPluginData>();
 
 			services.AddSingleton<MoviesSectionViewModel>();
 			services.AddSingleton<ActorsSectionViewModel>();
