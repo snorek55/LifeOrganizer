79577d36324b0d38bae16fb9cbae752384849835
snorek55 <40332063+snorek55@users.noreply.github.com>
2020-08-12 15:53:28 +0200

    Reorganized mapper config and created plugin data for mef

---
+++ Common/Setup/IProfilePluginData.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+using System.Collections.Generic;
+
+namespace Common.Setup
+{
+	public interface IProfilePluginData
+	{
+		public IEnumerable<Profile> Profiles { get; }
+	}
+}
