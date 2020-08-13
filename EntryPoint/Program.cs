e29bb24fbb4c1ccd38f8edc932d4688331a7b9b2
snorek55 <40332063+snorek55@users.noreply.github.com>
2020-08-13 17:40:59 +0200

    MEF implemented and adapted code accordingly Now Movorg is an extension.

--- EntryPoint/Program.cs
+++ EntryPoint/Program.cs
@@ -1,17 +1,18 @@
 using AutoMapper.Configuration;
 
+using Common.Adapters;
 using Common.Setup;
 
 using DesktopGui.Main;
 
 using EntryPoint.Setup;
 
-using MovOrg.Organizers.Adapters.Container;
-
 using Organizers;
 
 using System;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Windows;
 
 namespace WinFormsUI
@@ -25,26 +26,60 @@ namespace WinFormsUI
 		private static void Main()
 		{
 			var injector = new DependencyInjector();
-			var autoMapper = injector.Get<IAutoMapper>();
 
-			var infrastructureProfileData = new MovOrg.Infrastructure.Setup.ProfilePluginData();
-			var movOrgProfileData = new MovOrg.Organizer.Setup.ProfilePluginData();
-			var movOrgContainerData = injector.Get<ContainerPluginData>();
+			InitializeMapper(injector);
 
-			var configExpression = new MapperConfigurationExpression();
-			configExpression.AddProfiles(movOrgProfileData.Profiles);
-			configExpression.AddProfiles(infrastructureProfileData.Profiles);
-			autoMapper.CreateMapper(configExpression);
-
-			var mainVM = injector.Get<MainWindowViewModel>();
-			var mainMenuItem = injector.Get<ContainerPluginData>();
-			injector.App.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = movOrgContainerData.ResourceDictionaryUri });
-
-			mainVM.MainMenuItems.Add(mainMenuItem);
-
-			mainVM.SelectedItem = mainVM.MainMenuItems.FirstOrDefault();
+			AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
+			InitializeContainers(injector);
 
 			injector.App.Run(injector.Get<MainWindow>());
+		}
+
+		//WPF does not handle well resolving referenced resources when they are externally loaded.
+		//https://stackoverflow.com/questions/20495778/pack-uri-to-resources-in-referenced-assembly
+		private static Assembly ResolveAssembly(object sender, ResolveEventArgs e)
+		{
+			var addinAssembly = Assembly.GetExecutingAssembly();
+			var missingAssemblyName = new AssemblyName(e.Name);
+
+			// Sometimes the WPF assembly resolver cannot even find the executing assembly...
+			if (missingAssemblyName.FullName == addinAssembly.FullName)
+				return addinAssembly;
+
+			var addinFolder = Path.GetDirectoryName(addinAssembly.Location);
+			var dllPath = Path.Combine(addinFolder, missingAssemblyName.Name + ".dll");
+
+			// If we find the DLL in the add-in folder, load and return it.
+			if (File.Exists(dllPath))
+				return Assembly.LoadFrom(dllPath);
+
+			// nothing found
+			return null;
+		}
+
+		private static void InitializeContainers(DependencyInjector injector)
+		{
+			var mainVM = injector.Get<MainWindowViewModel>();
+
+			foreach (var containerData in injector.GetMany<IContainerPluginData>())
+			{
+				injector.App.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = containerData.ResourceDictionaryUri });
+				mainVM.MainMenuItems.Add(containerData);
+			}
+			mainVM.SelectedItem = mainVM.MainMenuItems.FirstOrDefault();
+		}
+
+		private static void InitializeMapper(DependencyInjector injector)
+		{
+			var autoMapper = injector.Get<IAutoMapper>();
+			var configExpression = new MapperConfigurationExpression();
+
+			foreach (var profileData in injector.GetMany<IProfilePluginData>())
+			{
+				configExpression.AddProfiles(profileData.Profiles);
+			}
+
+			autoMapper.CreateMapper(configExpression);
 		}
 	}
 }
--- MovOrg.Infrastructure/Properties/ResetMigrations.ps1
+++ MovOrg.Infrastructure/Properties/ResetMigrations.ps1
@@ -1,5 +1,16 @@
-﻿cd D:\Programacion\VS\LifeOrganizer\Infrastructure
+﻿function ThrowIfError
+{
+if(!$?)
+{
+throw "Failed"
+}
+}
+
+cd $PSScriptRoot
+cd ..
 dotnet ef database -v update 0
+ThrowIfError
 dotnet ef migrations -v remove --no-build
 dotnet ef migrations -v add InitialCreate
 dotnet ef database -v update
+
