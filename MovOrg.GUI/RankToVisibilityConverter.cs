7cce74dbc9297104b40b648c52af64966a72aed4
snorek55 <40332063+snorek55@users.noreply.github.com>
2020-08-24 16:27:04 +0200

    Rank is not visible if it is not in 250 top

---
+++ MovOrg.GUI/RankToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using Common.WPF;
+
+using System;
+using System.Globalization;
+using System.Windows;
+
+namespace MovOrg.GUI
+{
+	public class RankToVisibilityConverter : BaseConverter<RankToVisibilityConverter>
+	{
+		public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			int? rank = (int?)value;
+			if (rank.HasValue)
+				return Visibility.Visible;
+			else
+				return Visibility.Collapsed;
+		}
+
+		public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
